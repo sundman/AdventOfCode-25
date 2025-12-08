cbd572b7c2fbe762f6b5d895d31b396fb8b93341
Michael Sundman <sundman@sundman.at>
2025-12-08 23:33:58 +0100

    solving some 2021 days

---
+++ AoC2021/Days/04/Day.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    internal class Day04 : IDay
+    {
+        private List<int> _numbers = [];
+        private readonly Dictionary<int, int> _numberDict = [];
+        private readonly List<Bingo> _bingoSheets = [];
+
+        private class Bingo
+        {
+            private List<int> Numbers = [];
+
+            private List<int>[] RowsAndColumns = new List<int>[10];
+
+            public Bingo(int[,] numbers)
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    RowsAndColumns[i] = [];
+                }
+
+                for (var x = 0; x < 5; x++)
+                {
+                    for (var y = 0; y < 5; y++)
+                    {
+                        RowsAndColumns[x].Add(numbers[x, y]);
+                        RowsAndColumns[5 + y].Add(numbers[x, y]);
+                        Numbers.Add(numbers[x, y]);
+                    }
+                }
+            }
+
+            public int Score(List<int> drawnNumbers)
+            {
+                return Numbers.Where(x => !drawnNumbers.Contains(x)).Sum();
+            }
+
+            public int CalculateBingoRound(Dictionary<int, int> numberDict)
+            {
+                return RowsAndColumns.Min(list => list.Max(number => numberDict[number]));
+            }
+        }
+
+        public void ReadInput()
+        {
+
+            var filename = Debugger.IsAttached ? "Example.txt" : "Input.txt";
+            var data = File.ReadAllLines($"days/{GetType().Name.Substring(3)}/{filename}");
+
+            _numbers = data[0].Split(',').Select(int.Parse).ToList();
+
+            for (int x = 0; x < _numbers.Count; x++)
+            {
+                _numberDict[_numbers[x]] = x;
+            }
+
+            var i = 2;
+            while (data.Length > i)
+            {
+                int[,] numbers = new int[5, 5];
+                for (int y = 0; y < 5; y++)
+                {
+                    var rowNumbers = data[i + y].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                    for (int x = 0; x < 5; x++)
+                    {
+                        numbers[x, y] = rowNumbers[x];
+                    }
+                }
+                _bingoSheets.Add(new Bingo(numbers));
+                i += 6;
+            }
+
+        }
+
+
+
+        public decimal Part1()
+        {
+            Bingo? winning = null;
+            int? winningRound = null;
+            foreach (var sheet in _bingoSheets)
+            {
+                var result = sheet.CalculateBingoRound(_numberDict);
+                if (winningRound == null || result < winningRound)
+                {
+                    winningRound = result;
+                    winning = sheet;
+                }
+
+            }
+
+            return (decimal)_numbers[winningRound.Value] * winning.Score(_numbers.GetRange(0, winningRound.Value + 1));
+        }
+
+        public decimal Part2()
+        {
+            Bingo? winning = null;
+            int? winningRound = null;
+            foreach (var sheet in _bingoSheets)
+            {
+                var result = sheet.CalculateBingoRound(_numberDict);
+                if (winningRound == null || result > winningRound)
+                {
+                    winningRound = result;
+                    winning = sheet;
+                }
+
+            }
+
+            return (decimal)_numbers[winningRound.Value] * winning.Score(_numbers.GetRange(0, winningRound.Value + 1));
+            
+        }
+     
+    }
+}
---
+++ AoC2021/Days/08/Day.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+namespace ConsoleApp
+{
+    internal class Day08 : IDay
+    {
+        private List<int> input;
+
+        private List<List<string>> output = [];
+        private List<(List<string> complete, List<string> output)> completeLine = [];
+
+
+        public void ReadInput()
+        {
+
+            var filename = Debugger.IsAttached ? "Example.txt" : "Input.txt";
+            var data = File.ReadAllLines($"days/{GetType().Name.Substring(3)}/{filename}");
+
+            foreach (var line in data)
+            {
+                var parts = line.Split('|', StringSplitOptions.TrimEntries);
+                output.Add(parts[1].Split(' ').ToList());
+                completeLine.Add((parts[1].Split(' ').Concat(parts[0].Split(' ')).ToList(), parts[1].Split(' ').ToList()));
+
+            }
+
+        }
+
+        public decimal Part1()
+        {
+            List<int> validLengt = [2, 3, 4, 7];
+            return output.Sum(x => x.Count(y => validLengt.Contains(y.Length)));
+
+
+        }
+
+        //  aaaa    ....    aaaa    aaaa    ....
+        // b    c  .    c  .    c  .    c  b    c
+        // b    c  .    c  .    c  .    c  b    c
+        //  ....    ....    dddd    dddd    dddd
+        // e    f  .    f  e    .  .    f  .    f
+        // e    f  .    f  e    .  .    f  .    f
+        //  gggg    ....    gggg    gggg    ....
+        //    6      2       5        5      4 
+
+        //   5:      6:      7:      8:      9:
+        //  aaaa    aaaa    aaaa    aaaa    aaaa
+        // b    .  b    .  .    c  b    c  b    c
+        // b    .  b    .  .    c  b    c  b    c
+        //  dddd    dddd    ....    dddd    dddd
+        // .    f  e    f  .    f  e    f  .    f
+        // .    f  e    f  .    f  e    f  .    f
+        //  gggg    gggg    ....    gggg    gggg
+        //   5        6      3        7      6
+
+        public decimal Part2()
+        {
+            decimal totalSum = 0;
+            foreach (var complete in completeLine)
+            {
+                var line = complete.complete;
+                var charsForOne = line.First(x => x.Length == 2);
+                var charsForSeven = line.First(x => x.Length == 3);
+                var charsForFour = line.First(x => x.Length == 4);
+                var charsForEight = line.First(x => x.Length == 7);
+
+                var a = charsForSeven.First(ch => !charsForOne.Contains(ch));
+                var d = charsForFour.First(ch => line.Where(x => x.Length == 5).All(y => y.Contains(ch)));
+                var b = charsForFour.First(ch => ch != d && !charsForOne.Contains(ch));
+
+                var charsForNine = line.First(str => str.Length == 6 
+                                                     && str.Contains(d)
+                                                     && charsForOne.All(str.Contains));
+                var g = charsForNine.First(ch => !charsForOne.Contains(ch) && ch != a && ch != b && ch != d);
+                var e = charsForEight.First(ch =>
+                    !charsForOne.Contains(ch) && ch != a && ch != b && ch != d && ch != g);
+
+                List<int> digits = [];
+                foreach (var num in complete.output)
+                {
+                    int digit;
+
+                    // 1
+                    if (num.Length == 2)
+                        digit = 1;
+                    else if (num.Length == 3)
+                    {
+                        digit = 7;
+                    }
+                    else if (num.Length == 4)
+                    {
+                        digit = 4;
+                    }
+                    else if (num.Length == 7)
+                    {
+                        digit = 8;
+                    }
+                    // 2, 5, 3 
+                    else if (num.Length == 5)
+                    {
+                        if (num.Contains(e))
+                            digit = 2;
+                        else if (num.Contains(b))
+                            digit = 5;
+                        else
+                            digit = 3;
+                    }
+                    // 0,6,9
+                    else
+                    {
+                        if (!num.Contains(d))
+                            digit = 0;
+                        else if (num.Contains(e))
+                            digit = 6;
+                        else
+                            digit = 9;
+
+                    }
+                    digits.Add(digit);
+                }
+
+                totalSum += digits.Aggregate(0, (current, d) => current * 10 + d);
+            }
+
+
+
+
+            return totalSum;
+
+        }
+    }
+}
